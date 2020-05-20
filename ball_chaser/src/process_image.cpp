264122c0ad2e28418f9a6e76fe4246350d1c582e
fazlurnu <fazlur.rahman.ae@gmail.com>
2020-05-20 17:34:36 +0800

    add controller

--- ball_chaser/src/process_image.cpp
+++ ball_chaser/src/process_image.cpp
@@ -8,19 +8,55 @@ ros::ServiceClient client;
 // This function calls the command_robot service to drive the robot in the specified direction
 void drive_robot(float lin_x, float ang_z)
 {
-    // TODO: Request a service and pass the velocities to it to drive the robot
+    ROS_INFO_STREAM("Driving robot towards the ball");
+
+    ball_chaser::DriveToTarget srv;
+    srv.request.linear_x = lin_x;
+    srv.request.angular_z = ang_z;
+
+    if (!client.call(srv))
+        ROS_ERROR("Failed to call service safe_move");
 }
 
 // This callback function continuously executes and reads the image data
 void process_image_callback(const sensor_msgs::Image img)
 {
+    // The idea is to find the center of mass (in step direction) of the ball and use it
+    // to define whether the ball is on the left/center/right
 
     int white_pixel = 255;
+    int image_height = img.height;
+    int step = img.step;
+    float x_center_of_image = step/2;
+    
+    float command_lin_x = 0;
+    float command_ang_z = 0;
 
-    // TODO: Loop through each pixel in the image and check if there's a bright white one
-    // Then, identify if this pixel falls in the left, mid, or right side of the image
-    // Depending on the white ball position, call the drive_bot function and pass velocities to it
-    // Request a stop when there's no white ball seen by the camera
+    int total_mass = 0;
+    float sum_of_x = 0;
+
+    float kp = 2; //gain for turning
+
+    for(int i=0; i < image_height; i++){
+        for(int j=0; j < step; j++){
+            if(img.data[i*step + j] == white_pixel){
+                sum_of_x += j;
+                total_mass++;
+            }
+        }
+    }
+
+    float x_center_of_mass = sum_of_x/total_mass;
+
+    if(total_mass > 0){
+        command_lin_x = 0.1;
+        command_ang_z = (x_center_of_image - x_center_of_mass)/x_center_of_image * kp; //normalized
+    } else{
+        command_lin_x = 0.0;
+        command_ang_z = 0.0;
+    }
+
+    drive_robot(command_lin_x, command_ang_z);
 }
 
 int main(int argc, char** argv)
